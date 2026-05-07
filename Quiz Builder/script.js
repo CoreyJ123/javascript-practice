4c146f1841eea96a70f556eaa544f5c3f9a28b30
Corey <jonecj3@student.op.ac.nz>
2026-05-08 03:08:05 +1200

    added slightly more to Quiz Builder lab

--- Quiz Builder/script.js
+++ Quiz Builder/script.js
@@ -1,5 +1,7 @@
 const answers = {
-    q1: "Buddy"
+    q1: "Buddy",
+    q2: 8,
+    q3: 50
 }
 
 let playerScore = 0;
@@ -10,7 +12,9 @@ scoreDisplay.innerHTML = playerScore;
 // Get the answers from the form
 const quiz = document.querySelector("#quiz-form")
 quiz.addEventListener("submit", (e) => {
-    const q1answer = document.querySelector("#q1").value;
+    const q1answer = document.querySelector("#q1");
+    const q2answer = document.querySelector("#q2").value;
+    const q3answer = document.querySelector("#q3").value;
     e.preventDefault();
 
     // Compare them with the correct answers
@@ -20,6 +24,20 @@ quiz.addEventListener("submit", (e) => {
         scoreDisplay.innerHTML = playerScore;
     }else{
         console.log("Actually it's Buddy");
+    }
+    if(q2answer === answers.q2){
+        playerScore++;
+        console.log("Yes!! Your score is: " + playerScore);
+        scoreDisplay.innerHTML = playerScore;
+    }else{
+        console.log("Actually it's 8");
+    }
+    if(q3answer === answers.q3){
+        playerScore++;
+        console.log("Yes!! Your score is: " + playerScore);
+        scoreDisplay.innerHTML = playerScore;
+    }else{
+        console.log("Actually it's 50");
     }
 })
 
